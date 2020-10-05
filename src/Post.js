86d4cdabdb5c1f835e7823e0f0eca67cb5456d76
AhmedAbdelhak1 <ahmedabdelhak0@gmail.com>
2020-10-05 23:40:24 +0200

    add comment

--- src/Post.js
+++ src/Post.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {db} from './firebase';
 import './Post.css';
 import Avatar from "@material-ui/core/Avatar";
 
-function Post({ username, caption, imageUrl}) {
+
+function Post({ postId,username, caption, imageUrl}) {
     const [comments,setComments] = useState([]);
+    const [comment,setComment] = useState('');
+
+    useEffect(()=>{
+        let unsebscribe;
+        if(postId){
+            unsebscribe = db
+            .collection("posts")
+            .doc(postId)
+            .collection("comments")
+            .onsnapshot((snapshot =>{
+                setComments(snapshot.docs.map(doc =>doc.data()));
+            }));
+        }
+        return ()=>{
+            unsebscribe();
+        };
+
+    },[postId]);
+
+    const postComment = (event)=>{
+
+    }
+
     return (
         <div className="post">
             <div className="post__header">
@@ -13,7 +38,25 @@ function Post({ username, caption, imageUrl}) {
             
             <img className="post__image" src={imageUrl} alt=""/>
             <h4 className="post__text"><strong>{username}</strong>: {caption} </h4>
-            
+
+
+         <form className="post__commentBox">
+         <input 
+            className="post__input"
+            type="text"
+            placeholder="Add a comment..."
+            value={comment}
+            onChange={(e)=> setComment(e.target.value)}
+         />
+         <button className="post__button"
+         disabled={!comment}
+         type="submit"
+         onClick={postComment} > 
+            Post
+         </button>
+
+         </form>
+
         </div>
     )
 }
